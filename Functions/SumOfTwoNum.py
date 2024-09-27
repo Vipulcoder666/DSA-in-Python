2f5c17a55fb9c819e016a8797a56e65c8cc250f2
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-09-28 00:06:29 +0530

    updated

--- Functions/SumOfTwoNum.py
+++ Functions/SumOfTwoNum.py
@@ -1,7 +1,9 @@
 def Sum(n1,n2):
+    print("n1 : ",n1)
+    print("n2 : ",n2)
     ans = n1+n2
     return ans
-a=10
-b=10
-ans = Sum(a,b)
+# a=10
+# b=10
+ans = Sum(20,30)
 print("The sum of x and y is : ",ans)
