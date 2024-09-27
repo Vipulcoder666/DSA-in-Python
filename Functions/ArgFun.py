3faf9281fd52041023ebe31b4f16d510840b5e1b
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-09-28 00:32:34 +0530

    updated

--- Functions/ArgFun.py
+++ Functions/ArgFun.py
@@ -8,4 +8,6 @@ output = Sum(1,2,3,4,5)
 print("The sum is : ",output)
 
 # Arbitary arguments  are used where we dont know that how many parameters we have to enter in the function.
+
+# The imp thing is that the argument is taken as a tuple
 # we can add n numbers of parameter by using Arbitary arguments .
