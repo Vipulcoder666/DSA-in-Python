179579e8ea32871b579b9e5954b1e86de2c327bd
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-09-28 23:47:37 +0530

    Sum of ele without fun

--- Functions/SumOfTwoNum.py
+++ Functions/SumOfTwoNum.py
@@ -1,10 +1,18 @@
-def Sum(n1,n2):
-    print("n1 : ",n1)
-    print("n2 : ",n2)
-    ans = n1+n2
+# def Sum(n1,n2):
+#     print("n1 : ",n1)
+#     print("n2 : ",n2)
+#     ans = n1+n2
+#     return ans
+# # a=10
+# # b=10
+# ans = Sum(20,30)
+# print("The sum of x and y is : ",ans)
+# print("THe sum is : ",Sum(n1=10,n2=30))
+
+def sum(a,b):
+    print("a = ",a)
+    print("b = ",b)
+    ans = a+b
     return ans
-# a=10
-# b=10
-ans = Sum(20,30)
-print("The sum of x and y is : ",ans)
-print("THe sum is : ",Sum(n1=10,n2=30))
+ans = sum(10,20)
+print("The sum of {a} and {b} is : ",ans)
