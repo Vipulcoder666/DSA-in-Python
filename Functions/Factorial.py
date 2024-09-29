dc3914b963b23ccf04f5e35633bdb5d675a0a30b
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-09-29 23:24:02 +0530

    Factorial

--- Functions/Factorial.py
+++ Functions/Factorial.py
@@ -1,11 +1,11 @@
 def factorial(n):
-    ans = 1
+    answ = 1
     if n == 0:
-        ans = 1
+        answ = 1
     else:
         for i in range(1,n+1):
-            ans *= i
-        return ans
+            answ *= i
+        return answ
 
 
 n = int(input("Enter n : "))
