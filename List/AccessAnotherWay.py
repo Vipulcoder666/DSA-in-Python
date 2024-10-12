039b7235c0b702a96b2c95683695785e97ead54b
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-13 00:40:30 +0530

    li3

--- List/AccessAnotherWay.py
+++ List/AccessAnotherWay.py
@@ -1,3 +1,5 @@
+#easy method of accessing the elements of a list
+
 li = ['vipul',4,5,1,2,'shrivastav',6,5]
 for ele in li:
     print(ele)
---
+++ List/AccessAnotherWay2.py
@@ -0,0 +1,5 @@
+# access element of a list from random element
+
+li = [1,2,3,4,5,6]
+for ele in li[2:]:
+    print(ele)
