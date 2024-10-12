b53121a7eb41e466df23f917ea081591f9ebec77
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-13 00:32:30 +0530

    Access2

---
+++ List/Access2.py
@@ -0,0 +1,5 @@
+# access elements from index 2
+
+li = [4,3,5,'vipul',7,9,'python']
+for i in range (2,len(li)):
+    print(li[i])
--- List/AcessEleOfLi.py
+++ List/AcessEleOfLi.py
@@ -1,3 +1,5 @@
+# access elements of a list
+
 li = [1,2,3,'Parikh']
 for i in range (len(li)):
     print(li[i])
