6d354532bbe9a63ed883e9888f239b18d4ac17ee
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-17 20:21:37 +0530

    GFG 1st

---
+++ GFG/OddEven.py
@@ -0,0 +1,9 @@
+# Given a positive integer x. Your task is to check, if it is even or odd (Any number that gives zero as remainder when divided by 2 is an even number)
+
+def checkOddEven(x):
+    if(x % 2 == 0):
+      # Complete the statement below
+      return True 
+    else:
+        # Complete the statement below
+        return False
--- List/rough.py
+++ List/rough.py
@@ -1,2 +1,5 @@
-txt = "Hello World"[::-1]
-print(txt)
+# txt = "Hello World"[::-1]
+# print(txt)
+
+a = 1,2,3,4[::-1]
+print(a)
