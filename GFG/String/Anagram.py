a054af70eda177dc1e6207cdb3a8ac74cf9c30e4
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-21 23:34:38 +0530

    Anagram

---
+++ GFG/String/Anagram.py
@@ -0,0 +1,8 @@
+s1 = 'act'
+s2 = 'tac'
+
+if len(s1)!=len(s2):
+    print("False")
+else:
+    sorted(s1)==sorted(s2)
+    print("True")
--- GFG/rough.py
+++
@@ -1,5 +0,0 @@
-l1=[1,2,3,4,5]
-l2=[3,3,4,5,6,7]
-
-intersection = list(set(l1) & set(l2))
-print(intersection)
