c775c4b4bb112f18382233497021ec2e43aa0710
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-27 21:48:59 +0530

    updated

--- GFG/rough.py
+++ GFG/rough.py
@@ -1,7 +1,12 @@
-arr = [0, 2, 1, 2, 0]
-for i in range(len(arr)):
-    if arr[0] >= arr[i]:
-        arr[0] += 1
-    else:
-        arr[i] += 1
-print(arr)
+# arr = [1,2,1,1,5,2]
+# key = 0
+# for i in range(len(arr)):
+#     if arr[1] == arr[i]:
+#         key += 1
+# print(key)
+
+
+arr = [1,2,3,1,1,1,2,2]
+n = int(input("Enter the number to find : "))
+frq = arr.count(n)
+print(frq)
--- GFG/tempCodeRunnerFile.py
+++ GFG/tempCodeRunnerFile.py
@@ -1,12 +1,3 @@
-arr = [16, 17, 4, 3, 5, 2]
-count = arr[-1]
-leader = [count]
-print(count,end=' ')
-
-for i in range(len(arr) -2,-1,-1):
-    if arr[i] >= count:
-        count = arr[i]
-        leader.append(count)
-
-leader.reverse()
-print(leader,end=' ')
+# arr = [1,2,1,1,5,2]
+# key = 0
+# for i in range(len(arr)):
